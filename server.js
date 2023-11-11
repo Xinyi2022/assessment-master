085ef6498e6a39b2f4a14a3303859439c51411f3
Xinyi Li <xinyil8023@gmail.com>
2023-11-11 15:17:01 -0800

    Connect signin form with parent portal page
    - Added a new page for redirecting
    - Render the new page only if entered the password for the parent portal

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
+/**
+ * Module dependencies.
+ */
 const express = require('express');
 
 const PORT = 3000;
-
 const app = express();
+
+/**
+ * configs
+ */
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 
+
+/**
+ * APIs
+ */
 app.listen(PORT, () => {
   console.log(`server starts listening on port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
   res.render('index.html');
+});
+
+app.post('/parent_portal', (req, res) => {
+  console.log('password submitted:', req.body);
+  res.render('parent_portal.html');
 });
