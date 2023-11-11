52b9e4031cae5ca1925d400b59adba66df98561f
Xinyi Li <xinyil8023@gmail.com>
2023-11-11 11:19:34 -0800

    slideshow complete

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('And this is where the Javascript goes')
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide-image");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
