30faa8f6425c505048e71bb7e91372c8362e3729
Xinyi Li <xinyil8023@gmail.com>
2023-11-11 12:21:18 -0800

    Portal button modal completed
    - Added a modal for parent portal to signin
    - change close button to black when hover over to X button

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ console.log('And this is where the Javascript goes')
 let slideIndex = 0;
 showSlides();
 
+var modal = document.getElementById("sign-in-modal");
+
+var btn = document.getElementById("portal-button");
+
+var closeBtn = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 function showSlides() {
   let slides = document.getElementsByClassName("slide-image");
 
