56c345dcbe8ee6ac92f16e984802c54679894dce
Xinyi Li <xinyil8023@gmail.com>
2023-11-12 08:53:06 -0800

    used http-status-codes lib
    replaced plain http status codes with enum representations.

--- db/subscription-controllers.js
+++ db/subscription-controllers.js
@@ -1,37 +1,33 @@
 const Subscription = require('../db/Subsription');
 const { sendConfirmationEmail } = require('../mailer');
+const { StatusCodes } = require('http-status-codes');
 
-exports.getAll = (req, res) => {
-  Subscription.find({})
-    .then(data =>
-      res.status(200).send(data)
-    ).catch(err =>
-      res.status(400).send(err)
-    );
-};
-
-exports.add = async (req, res) => {
+exports.add = (req, res) => {
   const { email } = req.body;
   Subscription.where({
     email: email
   }).findOne()
     .then(record => {
       if (record != null) {
-        res.status(200)
+        res.status(StatusCodes.OK)
           .send("You are already on the subscription list!");
       } else {
         const newSubscriber = new Subscription(req.body);
         newSubscriber.save()
           .then(_ => {
             sendConfirmationEmail(email)
-              .then(_ => res.status(201)
-                .send("Subscribe success!"))
+              .then(_ => {
+                console.log('confirmation email has been sent to', email);
+                res.status(StatusCodes.CREATED)
+                  .send("Subscribe success!");
+              })
               .catch(err => console.log(err));
           })
-          .catch(err =>
-            res.status(500)
-              .send("Unable to subscribe")
-          );
+          .catch(err => {
+            console.log(err);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+              .send("Unable to subscribe");
+          });
       }
     });
 };
